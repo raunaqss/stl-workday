dfc3b31ddeff917e5af1331f409584a60770a1a7
raunaq <raunaq@spacecom.in>
2015-02-25 19:07:11 +0530

    Get the backend of SignUp and SignIn working

--- handlers.py
+++ handlers.py
@@ -2,11 +2,14 @@ import datetime
 import jinja2
 import logging
 import os
+import string
 import webapp2
 
 from utils import *
+from dbmodels import *
 
 from google.appengine.ext import db
+from google.appengine.api import memcache
 
 
 # initializing jinja2
@@ -28,7 +31,63 @@ class Handler(webapp2.RequestHandler):
 		self.write(self.render_str(template, **params))
 
 
-class MainPage(Handler):
+class ParentHandler(Handler):
+
+	def set_secure_cookie(self, cookie_name, val):
+		'''
+		Takes the name and val of the cookie.
+		Makes the secure value of the cookie by using the val in the input.
+		Sets the Cookie with the name provided and the secure cookie value.
+
+		cookie_name: String
+		nal = String
+		'''
+		cookie_val = make_secure_val(val)
+		self.response.headers.add_header('Set-Cookie',
+			"%s=%s; Path=/" % (cookie_name, cookie_val))
+
+	def read_secure_cookie(self, cookie_name):
+		'''
+		Returns the Value of the cookie (without the hash) if the cookie value
+		is valid.
+
+		Name: String
+		'''
+		browser_cookie = self.request.cookies.get(cookie_name)
+		#logging.info('browser cookie is %s' % browser_cookie)
+		return browser_cookie and check_secure_val(browser_cookie)
+
+	def login(self, user):
+		'''
+		Uses the funciton set_secure_cookie() to set the secure cookie value in
+		order to login the user.
+
+		user: User entity
+		'''
+		self.set_secure_cookie('user_id', str(user.key().id()))
+
+	def logout(self):
+		'''Sets the cookie to blank'''
+		self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
+
+	def initialize(self, *a, **kw):
+		'''
+		Overrides webapp2's initialize function. This function is run with
+		every request.
+		This function calls webapp2's initialize function to maintain important
+		functionality.
+
+		It reads secure val of the cookie: 
+		if it exists: 
+			it sets the corresponding user to the variable self.logged_in_user.
+		'''
+		webapp2.RequestHandler.initialize(self, *a, **kw)
+		uid = self.read_secure_cookie('user_id')
+		#logging.info('uid is %s' % uid)
+		self.logged_in_user = uid and User.get_user(uid)
+
+
+class MainPage(ParentHandler):
 
 	def write_login_form(self, email = "",
 							   username = "",
@@ -47,7 +106,10 @@ class MainPage(Handler):
 							 all_errors = all_errors)
 
 	def get(self):
-		self.write_login_form()
+		if not self.logged_in_user:
+			self.write_login_form()
+		else:
+			self.write('Welcome, ' + self.logged_in_user.username + '!')
 
 	def post(self):
 		signin = self.request.get('signin')
@@ -55,7 +117,14 @@ class MainPage(Handler):
 		logging.error('signin = ' + signin)
 		logging.error('signup = ' + signup)
 		if signin:
-			self.write_login_form()
+			username_or_email = self.request.get('username_or_email')
+			password = self.request.get('password')
+			user = User.valid_login(username_or_email, password)
+			if user:
+				self.login(user)
+				self.redirect('/')
+			else:
+				self.redirect('/login')
 		elif signup:
 			username = self.request.get('username')
 			email = self.request.get('email')
@@ -72,9 +141,35 @@ class MainPage(Handler):
 									  username = username,
 									  fullname = fullname,
 									  all_errors = all_errors)
+			else:
+				existing_user = User.get_user(email)
+				taken_username = User.get_user(username)
+				if existing_user or taken_username:
+					if existing_user:
+						all_errors[
+						'email_error'
+						] = "This email has already been registered."
+					if taken_username:
+						all_errors[
+						'username_error'
+						] = "This username is already taken."
+					self.write_login_form(email = email,
+										  username = username,
+										  fullname = fullname,
+										  all_errors = all_errors)
+				else:
+					new_user = User.register(username,
+											 email,
+											 fullname,
+											 password,
+											 profile_picture)
+					new_user.put()
+					new_user.set_user_caches()
+					self.login(new_user)
+					self.redirect('/')
 
 
-class LoginHandler(Handler):
+class LoginHandler(ParentHandler):
 
 	def get(self):
-		self.redirect('/')
+		self.write('Okay, thank god.')
--- utils.py
+++ utils.py
@@ -1,8 +1,126 @@
+import datetime
+import hashlib
+import hmac
+import logging
+import random
 import re
+import string
+
+from pytz.gae import pytz
+from google.appengine.api import memcache
+from secret import *
+
+def what_attribute(uid_or_username_or_email):
+	"""
+	Helper function for master Query function User.get_user().
+	It tells the function the attribute the user wants to query.
+	"""
+	if uid_or_username_or_email.isdigit():
+		return 'uid'
+	elif valid_email(uid_or_username_or_email):
+		return 'email'
+	else:
+		return 'username'
+
+def set_cache(cache_key, value):
+	"""
+	Sets memcache using Check & Set.
+	It uses only 'set' if the key is uninitialized.
+	"""
+	client = memcache.Client()
+	try:
+		while True:
+			old_value = client.gets(cache_key)
+			assert old_value, 'Uninitialized Key'
+			if client.cas(cache_key, value):
+				break
+	except AssertionError:
+		client.add(cache_key, value)
+		logging.error('Initializing Key')
 
 
-USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
-PASS_RE = re.compile(r"^.{3,20}$")
+def make_salt():
+	"""	
+	Returns a random 5 letter salt used for hashing the password.
+	Helper function for: make_pw_hash()
+
+	Input: NA
+	Returns: String
+	"""
+	return ''.join(random.choice(string.letters) for x in xrange(5))
+
+
+def aware_utcnow():
+	"""Get aware utcnow to store it in the date_createrd property."""
+	return datetime.datetime.utcnow().replace(tzinfo = pytz.UTC)
+
+
+def make_pw_hash(name, pw, salt = make_salt()):
+	"""
+	Returns the valid password hash: 'string of password hash','salt'
+	The hashed password has a comma , as the separator.
+
+	name: String
+	pw: String
+	Salt: String
+
+	Returns: String
+	"""
+	h = hashlib.sha256(name + pw + salt).hexdigest()
+	return '%s,%s' % (h, salt)
+
+
+def valid_pw(name, pw, h):
+	"""
+	Returns a boolean for whether the given password is valid.
+
+	name: String
+	pw: String
+	h: String
+	Returns: Boolean
+	"""
+	return h == make_pw_hash(name, pw, h.split(',')[-1])
+
+
+def hash_str(val):
+	"""
+	Helper function for make_secure_val(). Hashes the val with the SECRET in
+	the global environment.
+
+	val: String
+	Returns: String
+	"""
+	return hmac.new(SECRET, val).hexdigest()
+
+
+def make_secure_val(val):
+	"""
+	Makes the hashed cookie for a given val.
+	The hashed cookie uses a pipe | as the separator between the value and it's
+	hash.
+
+	val: String
+	Returns: String
+	"""
+	return '%(value)s|%(hashed)s' % {'value': val, 'hashed': hash_str(val)}
+
+
+def check_secure_val(h):
+	"""
+	Used to validate the hashed cookie when received from the browser in a
+	request.
+	if valid -> Returns the val of the cookie
+
+	h: String
+	Returns: String
+	"""
+	val = h.split('|')[0]
+	if h == make_secure_val(val):
+		return val
+
+
+USER_RE = re.compile(r"^[a-zA-Z][a-zA-Z0-9_-]{3,20}$")
+PASS_RE = re.compile(r"^.{6,20}$")
 EMAIL_RE = re.compile(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
 
 
